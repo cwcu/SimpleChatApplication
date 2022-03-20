b485063d96aec26383dbb58c81a4ca5ca1d886f6
cwcu <cw3277@columbia.edu>
2022-03-20 17:17:55 -0400

    de-registration & offline chat

--- ChatApp.py
+++ ChatApp.py
@@ -41,8 +41,20 @@ class Server:
 				port = clientAddress[1]				
 				self.addClient(name, IP, port)
 			elif message[0] == 5: # 5: message to save
-				# message[1] message[2]
-				pass
+				msg = message[1]
+				name = message[2]
+				requestName = message[3]
+				self.saveMessage(name, msg, requestName)
+			elif message[0] == 6: # 6: client deregistration request
+				name = message[1]
+				self.table[name]['status'] = 'no'
+				IP = self.table[name]['IP'] 
+				port = self.table[name]['port']
+				self.broadcast()
+				self.serverSocket.sendto(json.dumps([1]).encode(), (IP, port)) # send ack
+			elif message[0] == 7: # 7: client log-back request
+				name = message[1]
+				self.logBackClient(name)
 			else:
 				pass
 
@@ -58,12 +70,73 @@ class Server:
 				port = clientInfo['port']
 				self.serverSocket.sendto(json.dumps([2, self.table]).encode(), (IP, port)) # type 2 message: table
 
-
 	def addClient(self, name, IP, port):
 		# check duplicate client name -> error
 		self.table.update({name : {'IP' : IP, 'port' : port, 'name' : name, 'status' : 'yes'}})
 		self.broadcast()
 
+	def logBackClient(self, name):
+		if name not in self.table.keys():
+			raise Exception("invalid name for client log-back")
+
+		fileName = name + '.txt'
+		try:
+			offlineMessages = ['You Have Messages\n']
+			with open(fileName, 'r') as f:  
+				line = f.readline()
+				if not line:
+					# no offline message
+					raise Exception()
+				else:
+					# assume the memory is enough for all the offline messages for one client
+					# so we pack all the offline messages for a client and send them together
+					while line:
+						offlineMessages.append(line)
+						line = f.readline()
+
+					IP = self.table[name]['IP']
+					port = self.table[name]['port']
+					self.serverSocket.sendto(json.dumps([9, offlineMessages]).encode(), 
+						(IP, port)) # type 9: offline messages	
+
+			# clear all the offline messages for the client in the server
+			with open(fileName, 'w') as f:  
+				pass
+
+		except Exception:
+			pass
+			
+		self.table[name]['status'] = 'yes'
+		self.broadcast()
+
+	def saveMessage(self, name, message, requestName):
+		if name not in self.table.keys():
+			# print(<error message>) 
+			# return ?
+			raise Exception("nonexistent client") 
+
+		if requestName not in self.table.keys():
+			raise Exception("save-message request sent from nonexistent client")
+
+		sourceIP = self.table[requestName]['IP']
+		sourcePort = self.table[requestName]['port']
+
+		if self.table[name]['status'] == 'yes':
+			self.serverSocket.sendto(json.dumps([8, name, self.table]).encode(), (sourceIP, sourcePort)) # save-message error message
+		else:
+			self.table[name]['status'] = 'no'
+			self.broadcast()
+
+			fileName = name + '.txt'
+			with open(fileName, 'a') as f:  
+				savedMessage = requestName + ': ' + str(time.time()) + ' ' + message + '\n'
+				f.write(savedMessage)
+
+			self.serverSocket.sendto(json.dumps([1]).encode(), (sourceIP, sourcePort)) # ack
+
+	def __del__(self):
+		# delete all saved message files?
+		pass
 
 class Client:
 
@@ -76,6 +149,7 @@ class Client:
 		self.serverPort = serverPort
 		self.port = port # client port
 		self.table = {} # { <name> : {'IP' : <IP>, 'port' : <port>, 'name' : <name>, 'status' : <status>} }
+		self.shift = False # at current command line, whether there's '>>>' at the beginning
 
 		self.clientSocket = socket(AF_INET, SOCK_DGRAM) # ipv4, UDP
 		self.clientSocket.bind(('', port))	
@@ -85,14 +159,23 @@ class Client:
 		if message[0] == 2: # message type 2: updated table
 			print('>>> [Welcome, You are registered.]')		
 			self.table = message[1]
-			print('>>> [Client table updated.]')
-			# self.clientSocket.settimeout(0.5) # timeout = 500 msec	
-			listenThread = threading.Thread(target = self.listen).start()
+			print('>>> [Client table updated.]')	
+
+			self.listenThread = threading.Thread(target = self.listen) # create the listening thread
+			self.listenThread.daemon = True
+			self.listenThread.start()
+
 			while True:
+				self.shift = True 
 				command = input('>>> ')
 				command = command.split(' ')
+				self.shift = False # when a command is typed in and enter 'return', we get to a new line, so no '>>>' at the beginning
 				if command[0] == 'send':
 					self.send(command[1], command[2])
+				elif command[0] == 'dereg':
+					self.deregistration(command[1])
+				elif command[0] == 'reg':
+					self.logBack(command[1])
 		else: # registration request rejected
 			pass
 
@@ -108,7 +191,18 @@ class Client:
 				self.ack = True
 			elif message[0] == 2: # message type 2: updated table	
 				self.table = message[1]
-				print('[Client table updated.]\n>>> ', end = '')
+				if self.shift:
+					print('[Client table updated.]\n>>> ', end = '')
+				else: 
+					print('>>> [Client table updated.]')
+			elif message[0] == 8: # message type 8: save-message error message
+				print(f'>>> [Client {message[1]} exists!!]')
+				self.table = message[2]
+				print('>>> [Client table updated.]')
+			elif message[0] == 9: # message type 9: offline messages
+				offlineMessages = message[1]
+				for line in offlineMessages:
+					print(line + '>>> ', end = '')
 			else: # other message types to be implemented
 				pass		
 
@@ -116,6 +210,8 @@ class Client:
 
 	def send(self, name, message):
 		'''
+		one client sends message to another client directly
+
 		implementation idea: we have two threads: the main thread (for user input command) and 
 		the listening thread (for receiving messages from the server and other clients). In the main 
 		thread, we first set self.ack = False, and then send the message to the target client, wait 
@@ -125,7 +221,14 @@ class Client:
 		'''
 
 		if name not in self.table.keys():
+			# exception -> print error message after '>>> '  
 			raise Exception("target client doesn't exist")
+
+		if self.table[name]['status'] == 'no':
+			# the recipient is offline (exit via notified leave)
+
+			self.saveMessageRequest(name, message)
+			return
 
 		IP = self.table[name]['IP']
 		port = self.table[name]['port']
@@ -134,14 +237,52 @@ class Client:
 		try:
 			time.sleep(0.5) # sleep for 500 msec
 			if self.ack:
-				print(f'[Message received by {name}.]\n>>> ', end = '')
+				print(f'>>> [Message received by {name}.]\n', end = '')
 			else:
 				raise Exception()
 		except:
 			# timeout, send the message to server
-			self.clientSocket.sendto(json.dumps([5, message, name]).encode(), 
-				(self.serverIP, self.serverPort)) # type 5: message	for the server to save
-			print(f'[No ACK from {name}, message sent to server.]\n>>> ', end = '')
+			print(f'>>> [No ACK from {name}, message sent to server.]')
+			self.saveMessageRequest(name, message)
+
+	def deregistration(self, name):
+		if name != self.name:
+			raise Exception("invalid name for de-registration")
+
+		self.ack = False
+		self.clientSocket.sendto(json.dumps([6, name]).encode(), (self.serverIP, self.serverPort)) # 6: de-registration request
+		time.sleep(0.5) # the listening thread is expecting an ack from the server
+		if self.ack: 
+			print('>>> [You are Offline. Bye.]\n', end = '')
+			return
+		else:
+			# didn't receive an ack, retry for 5 times
+			for i in range(5):
+				self.clientSocket.sendto(json.dumps([6, name]).encode(), (self.serverIP, self.serverPort))	
+				time.sleep(0.5)
+				if self.ack:
+					print('>>> [You are Offline. Bye.]\n', end = '')
+					return
+
+		print('>>> [Server not responding]\n>>> [Exiting]', end = '')
+		self.clientSocket.close()
+		sys.exit()
+
+	def saveMessageRequest(self, name, message):
+		self.ack = False
+		self.clientSocket.sendto(json.dumps([5, message, name, self.name]).encode(), 
+			(self.serverIP, self.serverPort)) # type 5: message	for the server to save		
+		# wait for 500 msec by default
+		time.sleep(0.5)
+		if self.ack:
+			print('>>> [Messages received by the server and saved]')
+		# what if unsuccessful save-message request, other than the recipient is online?
+
+	def logBack(self, name):
+		if name != self.name:
+			raise Exception("invalid name for log-back")
+
+		self.clientSocket.sendto(json.dumps([7, name]).encode(), (self.serverIP, self.serverPort)) # 7: client log-back request		
 
 	def __del__(self):
 		'''
--- README.md
+++ README.md
@@ -10,7 +10,9 @@ The clients and the server would maintain a table with information of clients, a
 { <name> : {'IP' : <IP>, 'port' : <port>, 'name' : <name>, 'status' : <status>} }
 ```
 
-i.e. a dict, where the value of each entry is also a dict recording the IP, port ,name and status of a client, and each client's name serves as the corresponding key.
+i.e. a dict, where the value of each entry is also a dict recording the IP, port , name and status of a client, and each client's name serves as the corresponding key.
+
+status = 'yes' or 'no' (indicating the client is online or offline)
 
 ### Message
 
@@ -22,12 +24,18 @@ i.e. a dict, where the value of each entry is also a dict recording the IP, port
 
 #### **message types:**
 
-message is in the format of [\<type\>, \<message\>, ...], \<type\> =
+message is in the format of [\<type\>, \<message\>, \<name\>, \<table\>, ...]
+
+\<type\> =
 
 - 0: ordinary message. e.g. [0, "Hello"]
 - 1: ack, where in this case the message = [1], i.e. no \<message\> part
 - 2: a table with information of clients. [2, \<table\>]
 - 3: client registration request. [3, \<name\>]
 - 4: client registration request rejected. [4] 
-- 5: message for the server to save. [5, \<message\>, \<name\>]
+- 5: save-message request. [5, \<message\>, \<name\>, \<requestName\>], where \<requestName\> is the name of the client that sends the save-message request
+- 6: client deregistration request. [6, \<name\>]
+- 7: client log-back request. [7, \<name\>]
+- 8: save-message error message from the server. [8, \<name\>, \<table\>], where \<name\> is the name of the intended recipient.
+- 9: offline messages. [9, \<offline messages\>]
 
